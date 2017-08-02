0b073988de78a37b8c194ee78bd14165a5b90583
walter <buczacka.walter@gmail.com>
2017-08-02 15:02:38 -0300

    set up and initial config

---
+++ app.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import config from'./config/config';
+import datasource from './config/datasource';
+import parser from 'body-parser';
+const app = express();
+
+app.config = config;
+app.datasource = datasource(app);
+app.set('port', 7000)
+app.use(parser.json())
+
+const Books = app.datasource.models.Books//otra forma de acceder al array
+
+app.route('/books')
+  .get((req, res) => {
+    Books.findAll({})
+      .then(result => res.json(result))
+      .catch(err => res.status(412))
+  })
+  .post((req, res) => {
+    Books.create(req.body)
+      .then(result => res.json(result))
+      .catch(err => res.status(412))
+  })
+
+app.route('/books/:id')
+  .get((req, res) => {
+    Books.findOne({where: req.params})
+      .then(result => res.json(result))
+      .catch(err => res.status(412))
+  })
+  .put((req, res) => {
+    Books.update(req.body, {where: req.params})
+    .then(result => res.json(result))
+    .catch(err => res.status(412))
+  })
+  .delete((req,res) => {
+    Books.destroy ({where: req.params})
+    .then(result => res.sendStatus(204)//success without response
+    .catch(err => res.status(412))
+  })
+
+export default app;
---
+++ config/datasource.js
@@ -0,0 +1,37 @@
+import Sequelize from 'sequelize'
+import fs from 'fs'
+import path from 'path'
+
+let database = null
+
+const loadModels = (sequelize) => {
+  const dir = path.join(__dirname, '../models')
+  let models = [], model;
+  //replace with promises.
+  // When we have large model files and lots of them, better approach load them async.
+  fs.readdirSync(dir).forEach(file => {
+    // composition ?
+    const modelDir = path.join(dir, file)
+    model = sequelize.import(modelDir)
+    models[model.name] = model
+  })
+  return models
+}
+
+export default (app) => {
+  if(!database) {
+    const config = app.config,
+    sequelize = new Sequelize(config.database, config.username, config.password, config.params)
+    database = {
+      sequelize,
+      Sequelize,
+      models: {}
+    }
+
+    database.models = loadModels(sequelize)
+    sequelize.sync().done(() => {
+      return database
+    })
+  }
+  return database
+}
