c60f8738f198b3b986c35a78db6cf0c88837ee85
walter <buczacka.walter@gmail.com>
2017-08-02 15:02:56 -0300

    add models for the API

---
+++ models/Books.js
@@ -0,0 +1,16 @@
+export default (sequilize, DataType) => {
+  return sequilize.define('Books', {
+    id: {
+      type: DataType.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataType.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    }
+  })
+}
