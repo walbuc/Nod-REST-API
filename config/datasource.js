68bd2fa8a9c59406127a84f709161afcede79384
walter <buczacka.walter@gmail.com>
2017-08-09 09:27:31 -0300

    apply code standards

--- config/datasource.js
+++ config/datasource.js
@@ -1,37 +1,38 @@
-import Sequelize from 'sequelize'
-import fs from 'fs'
-import path from 'path'
+import Sequelize from 'sequelize';
+import fs from 'fs';
+import path from 'path';
 
-let database = null
+let database = null;
 
 const loadModels = (sequelize) => {
-  const dir = path.join(__dirname, '../models')
-  let models = [], model;
-  //replace with promises.
-  // When we have large model files and lots of them, better approach load them async.
-  fs.readdirSync(dir).forEach(file => {
+  const dir = path.join(__dirname, '../models');
+  const models = [];
+  let model;
+  // replace with promises.
+  // When we have large model files and lots of them,
+  // better approach load them async.
+  fs.readdirSync(dir).forEach((file) => {
     // composition ?
-    const modelDir = path.join(dir, file)
-    model = sequelize.import(modelDir)
-    models[model.name] = model
-  })
-  return models
-}
+    const modelDir = path.join(dir, file);
+    model = sequelize.import(modelDir);
+    models[model.name] = model;
+  });
+  return models;
+};
 
 export default (app) => {
-  if(!database) {
-    const config = app.config,
-    sequelize = new Sequelize(config.database, config.username, config.password, config.params)
+  if (!database) {
+    const config = app.config;
+    const sequelize = new Sequelize(config.database, config.username,
+      config.password, config.params);
     database = {
       sequelize,
       Sequelize,
-      models: {}
-    }
+      models: {},
+    };
 
-    database.models = loadModels(sequelize)
-    sequelize.sync().done(() => {
-      return database
-    })
+    database.models = loadModels(sequelize);
+    sequelize.sync().done(() => database);
   }
-  return database
-}
+  return database;
+};
