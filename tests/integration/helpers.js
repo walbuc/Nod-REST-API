2937584ee3802b49cec8c218ddc1fa837732abff
walter <buczacka.walter@gmail.com>
2017-08-09 09:43:37 -0300

    update integration tests

--- tests/integration/helpers.js
+++ tests/integration/helpers.js
@@ -1,8 +1,8 @@
-import supertest from 'supertest'
-import chai from 'chai'
-import app from '../../app'
+import supertest from 'supertest';
+import chai from 'chai';
+import app from '../../app';
 
-//global variables
-global.app = app
-global.request = supertest(app)
-global.expect = chai.expect
+// global variables
+global.app = app;
+global.request = supertest(app);
+global.expect = chai.expect;
