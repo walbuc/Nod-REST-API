556e0b1fb1fc7efc13bf13546c4f6e5fef8b4f88
walter <buczacka.walter@gmail.com>
2017-08-23 11:34:28 -0300

    add controller logic

--- controllers/books.js
+++ controllers/books.js
@@ -1,10 +1,13 @@
+
+import HttpStatus from 'http-status';
+
 // return an object with arrow functions
-const defaultResponse = (data, statusCode = 200) => ({
+const defaultResponse = (data, statusCode = HttpStatus.OK) => ({
   data,
   statusCode,
 });
 
-const errorResponse = (message, statusCode = 400) => {
+const errorResponse = (message, statusCode = HttpStatus.BAD_REQUEST) => {
   defaultResponse({ error: message }, statusCode);
 };
 
@@ -15,31 +18,32 @@ class BooksController {
   getAll() {
     return this.Books.findAll({})
       .then(result => defaultResponse(result))
-      .catch(result => errorResponse(result, 412));
+      .catch(error => errorResponse(error.message));
   }
 
   getById(params) {
     return this.Books.findOne({ where: params })
       .then(result => defaultResponse(result))
-      .catch(result => errorResponse(result, 412));
+      .catch(error => errorResponse(error.message));
   }
 
   create(data) {
     return this.Books.create(data)
-      .then(result => defaultResponse(result, 201)) // resource created
-      .catch(result => errorResponse(result, 422));// can not be processed
+      .then(result => defaultResponse(result, HttpStatus.CREATED)) // resource created
+      .catch(error => errorResponse(error.message, HttpStatus.UNPROCESSABLE_ENTITY));//422 can not be processed
   }
 
   update(data, params) {
     return this.Books.update(data, { where: params })
       .then(result => defaultResponse(result))
-      .catch(result => errorResponse(result, 422));
+      .catch(error => errorResponse(error, HttpStatus.UNPROCESSABLE_ENTITY));
   }
 
   delete(params) {
     return this.Books.destroy({ where: params })
-      .then(result => defaultResponse(result, 204))
-      .catch(result => errorResponse(result, 422));
+      .then(result => defaultResponse(result, HttpStatus.NO_CONTENT))//204 successful response no content
+      .catch(error => errorResponse(error, HttpStatus.UNPROCESSABLE_ENTITY));
   }
 }
+
 export default BooksController;
