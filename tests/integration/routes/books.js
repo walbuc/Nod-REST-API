ad2d3eb7f15b702f2122683d65e2a49486a24acd
walter <buczacka.walter@gmail.com>
2017-09-13 11:27:56 -0300

    add user token authentication

--- tests/integration/routes/books.js
+++ tests/integration/routes/books.js
@@ -1,24 +1,42 @@
+import jwt from 'jwt-simple';
 describe('Testing routes for books resource', () => {
   const Books = app.datasource.models.Books;
+  const Users = app.datasource.models.Users;
+  const jwtSecret = app.config.jwtSecret;
+  let token;
+
   const Book = {
     id: 1,
     name: 'ThisIsABook',
+    description: 'This is the description',
   };
 
   beforeEach((done) => {
-    Books.destroy({ where: {} })
-      .then(() => {
-        Books.create(Book);
+    Users.destroy({ where: {} })
+      .then(() =>
+        Users.create({
+          name: "test user",
+          email: "test@mail.com",
+          password: "password"
+        }))
+      .then(user => {
+        Books.destroy({ where: {} })
+          .then(() => {
+            Books.create(Book);
+          })
+          .then(() => {
+            token = jwt.encode({id: user.id}, jwtSecret )
+            done();
+          });
       })
-      .then(() => {
-        done();
-      });
   });
 
   describe('Route GET/books', () => {
     it('Should return a list of books', (done) => {
+      //fijarme si request.get retorna una promise cambiarlo
       request
         .get('/books')
+        .set('Authorization', `bearer ${token}`)
         .end((err, response) => {
           expect(response.body[0].id).to.be.eql(Book.id);
           expect(response.body[0].name).to.be.eql(Book.name);
@@ -31,6 +49,7 @@ describe('Testing routes for books resource', () => {
     it('Should return a book', (done) => {
       request
         .get('/books/1')
+        .set('Authorization', `bearer ${token}`)
         .end((err, response) => {
           expect(response.body.id).to.be.eql(Book.id);
           expect(response.body.name).to.be.eql(Book.name);
@@ -47,8 +66,11 @@ describe('Testing routes for books resource', () => {
       };
       request
         .post('/books')
+        .set('Authorization', `bearer ${token}`)
         .send(NewBook)
         .end((err, response) => {
+          console.log(err)
+          console.log(response)
           expect(response.body.id).to.be.eql(NewBook.id);
           expect(response.body.name).to.be.eql(NewBook.name);
           done(err);
@@ -64,9 +86,9 @@ describe('Testing routes for books resource', () => {
       };
       request
         .put('/books/1')
+        .set('Authorization', `bearer ${token}`)
         .send(UpdateBook)
         .end((err, response) => {
-        // console.log('RESPONSE', response.body)
           expect(response.body).to.be.eql([1]);
           done(err);
         });
@@ -77,6 +99,7 @@ describe('Testing routes for books resource', () => {
     it('Should update a book', (done) => {
       request
         .delete('/books/1')
+        .set('Authorization', `bearer ${token}`)
         .end((err, response) => {
           expect(response.statusCode).to.be.eql(204);
           done(err);
